5be5d9bdaf2101e46481ef6bac3c56b560fe897c
aditbagja <aditbagja44@gmail.com>
2023-06-06 18:48:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        anton: ['Anton', ...defaultTheme.fontFamily.serif],
+        
+      },
+      colors:{
+        primary: '#eb3f33',
+        background: '#f3f3f3'
+      }
+    }
+  },
+  plugins: [],
+}
+

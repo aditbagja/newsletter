537c71033aaf53dccaccedd0ef7a1960ec343f04
aditbagja <aditbagja44@gmail.com>
2023-06-07 17:13:37 +0700

    update content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,19 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["index.html"],
+  mode: 'jit',
   theme: {
     extend: {
       fontFamily:{
         anton: ['Anton', ...defaultTheme.fontFamily.serif],
-        
       },
       colors:{
         primary: '#eb3f33',
         background: '#f3f3f3'
-      }
-    }
+      },
+    },
   },
+  variants: {},
   plugins: [],
 }
 
